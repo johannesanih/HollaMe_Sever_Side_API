a45703de4d4c7358caba8243dda89a605466e2ff
Johannes Anih <107084872+johannesanih@users.noreply.github.com>
2025-06-06 02:27:17 +0000

    did the post model added a lot of many to many fields

--- HollaME_Serverside_API/hollame/models.py
+++ HollaME_Serverside_API/hollame/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from users.models import CustomUser
+
+#post
+#messages
+#notifications
 
 # Create your models here.
 class Post(models.Model):
@@ -7,14 +12,63 @@ class Post(models.Model):
         ('COMMENT', 'Comment')
     ]
 
-    body = models.TextField(blank=False)
-    views = models.IntegerField(default=0, blank=True)
-    likes = models.IntegerField(default=0, blank=True)
-    shares = models.IntegerField(default=0, blank=True)
-    comment_count = models.IntegerField(default=0, blank=True)
+    content = models.TextField(blank=False)
     post_type = models.CharField(choices=post_type_enum, blank=False)
-    tags = models.TextField(blank=True)
-    # .. attached_to
-    author = models.ForeignKey('users.models.CustomUser', related_name='posts', on_delete=models.CASCADE)
+    child_post = models.ForeignKey(
+        'self',
+        related_name = 'threads',
+        on_delete=models.SET_NULL,
+        null = True,
+        blank = True
+    )
+    author = models.ForeignKey(CustomUser, related_name='posts', on_delete=models.CASCADE)
+    appearance_count = models.IntegerField(default=0)
+    likes = models.ManyToManyField(
+        CustomUser,
+        related_name = 'liked_posts',
+        blank = True,
+        through = 'PostLike'
+    )
+    shares = models.ManyToManyField(
+        CustomUser,
+        related_name = 'shared_posts',
+        blank = True,
+        through = 'PostShare'
+    )
+    seen_by = models.ManyToManyField(
+        CustomUser,
+        related_name = 'seen_posts',
+        blank = True,
+        through = 'PostSeen'
+    )
+    user_tags = models.ManyToManyField(
+        CustomUser,
+        related_name = 'tagged_in_posts',
+        blank = True,
+        through = 'PostUserTag'
+    )
     posted_on = models.DateTimeField(auto_now_add=True)
     edited_on = models.DateTimeField(auto_now=True)
+
+    def __str__ (self):
+        return f"HollaMe {self.post_type} - \"{self.content[:30]} ...\" - {self.author.username}"
+
+class PostUserTag(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tagged_on = models.DateTimeField(auto_now_add=True)
+
+class PostLike(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    liked_on = models.DateTimeField(auto_now_add=True)
+
+class PostShare(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    shared_on = models.DateTimeField(auto_now_add=True)
+
+class PostSeen(models.Model):
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    seen_on = models.DateTimeField(auto_now_add=True)
--- HollaME_Serverside_API/users/models.py
+++ HollaME_Serverside_API/users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+#user profile
+# -- followers
+
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
